7e7dbf6c598841d4dea29b9cf3062db0fcf0a9a4
鎌田正聖 <masatoshi@masa-macbook-pro.local>
2020-07-24 00:18:15 +0900

    Done

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -4,5 +4,24 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        m: usize,
+        i: [(usize, usize); m],
     }
+    let mut one_to_i = vec![false; n];
+    let mut i_to_n = vec![false; n];
+    for a in 0..m {
+        if i[a].0 == 1 {
+            one_to_i[i[a].1 - 1] = true;
+        } else if i[a].1 == n {
+            i_to_n[i[a].0 - 1] = true;
+        }
+    }
+    for j in 0..n {
+        if one_to_i[j] == true && i_to_n[j] == true {
+            println!("POSSIBLE");
+            return;
+        }
+    }
+    println!("IMPOSSIBLE");
 }
